78e6b2bb890fd8db586fb2455bcc08d99874bb73
Marty BlackBai <itblackbai@gmail.com>
2023-08-22 13:58:57 +0300

    add roles

---
+++ webSklad/Controllers/RolesController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using webSklad.Models;
+using webSklad.Models.ViewModels;
+
+namespace webSklad.Controllers
+{
+    public class RolesController : Controller
+    {
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<User> _userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            List<IdentityRole> model = _roleManager.Roles.ToList();
+            return View(model);
+        }
+
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string Name)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(Name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(Name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult UserList()
+        {
+            return View(_userManager.Users.ToList());
+        }
+
+
+        public async Task<IActionResult> Edit(string userId)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                ChangeRoleViewModel model = new ChangeRoleViewModel
+                {
+                    UserEmail = user.Email,
+                    UserId = user.Id,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string userId, List<string> roles)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var addedRoles = roles.Except(userRoles);
+                var removedRoles = userRoles.Except(roles);
+
+                await _userManager.AddToRolesAsync(user, addedRoles);
+                await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+                return RedirectToAction("UserList");
+
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string userId)
+        {
+            User user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                IdentityResult result = await _userManager.DeleteAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("UserList");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Failed to delete the user.");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "User not found.");
+            }
+            return View("UserList", _userManager.Users.ToList());
+        }
+
+    }
+}
